885a547000cd89f7f42d82df2ffe72df1ba99659
TheSarangal <info.sarangal@gmail.com>
2019-12-12 01:57:56 +0530

    Working on Advance Spantastic Library

--- app/src/main/java/in/sarangal/spannabletextview/MainActivity.java
+++ app/src/main/java/in/sarangal/spannabletextview/MainActivity.java
@@ -3,6 +3,10 @@ package in.sarangal.spannabletextview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import in.sarangal.lib.spantastic.Spantastic;
 
@@ -12,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String sentence = "Simple spannable with .";
+
+        List<String> strings = new ArrayList<>();
+        strings.add("spannable");
+        strings.add("underline");
+
+        TextView tvOne = findViewById(R.id.tv_one);
+        new Spantastic.SpantasticBuilder(this, tvOne, sentence)
+                .setSpanList(strings)
+                .showUnderline(true)
+                .apply();
+
+        TextView tvTwo = findViewById(R.id.tv_two);
+        new Spantastic.SpantasticBuilder(this, tvTwo, sentence)
+                .setSpanList(strings)
+                .showUnderline(true)
+                .apply();
+
+        TextView tvThree = findViewById(R.id.tv_three);
+
+        TextView tvFour = findViewById(R.id.tv_four);
+
+        TextView tvFive = findViewById(R.id.tv_five);
+
+        TextView tvSix = findViewById(R.id.tv_six);
+
     }
 }
